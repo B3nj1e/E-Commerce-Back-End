dcd85ec551ce82f2d281e76d9f7406e20ac45632
Benjamin Clewer <benjiclewer@gmail.com>
2021-12-02 13:51:54 +1030

    sequilize model index completed

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category)
 
 // Categories have many Products
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product-tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag-product'
+});
 
 module.exports = {
   Product,
