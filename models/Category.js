cad0b7c86b4374436e69eb11424a409b8f66b089
Benjamin Clewer <benjiclewer@gmail.com>
2021-12-03 10:36:44 +1030

    functioning database, functioning seed population, next step is routes

--- models/Category.js
+++ models/Category.js
@@ -13,7 +13,7 @@ Category.init(
       primaryKey: true,
       autoIncrement: true
     },
-    name: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false
     },
@@ -23,7 +23,7 @@ Category.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: 'Category',
   }
 );
 
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -15,7 +15,7 @@ ProductTag.init(
     product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'product',
+        model: 'Product',
         key: 'id',
         unique: false
       }
@@ -23,7 +23,7 @@ ProductTag.init(
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'tag',
+        model: 'Tag',
         key: 'id',
         unique: false
       }
@@ -34,7 +34,7 @@ ProductTag.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: 'ProductTag',
   }
 );
 
